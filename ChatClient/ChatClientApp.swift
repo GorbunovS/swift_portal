293e5fa4211de136f9038ab13f0a643db7f132b4
GorbunovS <obes1stas1@gmail.com>
2025-03-17 14:32:47 +0300

    Initial Commit

---
+++ ChatClient/ChatClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatClientApp.swift
+//  ChatClient
+//
+//  Created by Станислав Горбунов on 17.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChatClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatClientTests/ChatClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatClientTests.swift
+//  ChatClientTests
+//
+//  Created by Станислав Горбунов on 17.03.2025.
+//
+
+import Testing
+@testable import ChatClient
+
+struct ChatClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
